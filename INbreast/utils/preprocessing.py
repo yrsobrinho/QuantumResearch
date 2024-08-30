d920b4c14e2777d576819f27b1adc04fe343bc86
Yasmin Sobrinho <yasminrsobrinho@gmail.com>
2024-08-29 22:46:55 -0300

    Updated preprocessing.py

--- INbreast/utils/preprocessing.py
+++ INbreast/utils/preprocessing.py
@@ -6,7 +6,8 @@ import pandas as pd
 import scipy.ndimage
 from skimage import filters
 from sklearn.model_selection import train_test_split
-from PIL import Image
+import skimage
+from skimage.color import rgb2gray
 
 
 def preprocess_and_save_images(csv_file, root_dir, output_dir, size=224, test_size=0.2):
@@ -20,19 +21,15 @@ def preprocess_and_save_images(csv_file, root_dir, output_dir, size=224, test_si
     data['Label'] = data['Bi-Rads'].apply(lambda x: 0 if x in ['1', '2'] else 1)
     print("Labels atribuídos com base no Bi-Rads.")
 
-    # Convertendo a coluna 'File Name' para string
     data['File Name'] = data['File Name'].astype(str)
 
-    # Dividir os dados em conjuntos de treino e teste
     print("Dividindo os dados em conjuntos de treino e teste...")
     train_data, test_data = train_test_split(data, test_size=test_size, stratify=data['Label'], random_state=42)
     print(f"Total de imagens no treino: {len(train_data)}, no teste: {len(test_data)}")
 
-    # Criar um dicionário para mapear o nome dos arquivos aos rótulos
     train_labels = dict(zip(train_data['File Name'], train_data['Label']))
     test_labels = dict(zip(test_data['File Name'], test_data['Label']))
 
-    # Função para processar e salvar imagens
     def process_and_save(dataset, split, labels_dict):
         print(f"Processando o conjunto de {split}...")
 
@@ -52,58 +49,22 @@ def preprocess_and_save_images(csv_file, root_dir, output_dir, size=224, test_si
                 continue
 
             print(f"Processando imagem DICOM: {dicom_file}")
-            image = pydicom.dcmread(dicom_path).pixel_array
+            img, img_shape = read_dicom_img(dicom_path)
 
-            # Pré-processamento da imagem
-            image = histogram_equalization(image)
-            bin_img = preprocess_before_crop(image)
-            img_mask = get_mask_of_largest_connected_component(bin_img)
-            cropped_img = crop_img(image, img_mask)
+            proc_img = Image_procescing_mias(img)
 
-            # Converter a imagem para PIL antes de aplicar as transformações
-            image = Image.fromarray(cropped_img)
+            resized_img = cv2.resize(proc_img, (size, size))
 
-            # Redimensionar a imagem
-            image = image.resize((size, size))
-
-            # Converter a imagem de volta para numpy array
-            image = np.array(image)
-
-            # Obter o rótulo a partir do dicionário
             label = labels_dict[file_name]
             print(f"Rótulo para {file_name}: {label}")
 
-            # Salvar a imagem no diretório correspondente
             label_dir = os.path.join(split_output_dir, str(label))
             output_path = os.path.join(label_dir, f"{file_name}.png")
-            cv2.imwrite(output_path, image)
+            cv2.imwrite(output_path, resized_img * 255)  # Multiplicando por 255 para garantir que a imagem esteja em escala de 0-255
             print(f"Imagem salva em: {output_path}")
 
-    # Processar e salvar imagens para treino e teste
     process_and_save(train_data, 'train', train_labels)
     process_and_save(test_data, 'test', test_labels)
-
-
-def histogram_equalization(img):
-    if np.max(img) > 0:
-        m = int(np.max(img))
-        hist = np.histogram(img, bins=m + 1, range=(0, m + 1))[0]
-        hist = hist / img.size
-        cdf = np.cumsum(hist)
-        s_k = (255 * cdf).astype(np.uint8)
-        img_new = np.array([s_k[i] for i in img.ravel()]).reshape(img.shape)
-        return img_new
-    else:
-        return img
-
-
-def preprocess_before_crop(img):
-    threshold = filters.threshold_sauvola(img)
-    binary_img = (img > threshold) * 1
-    kernel = np.ones((5, 5), np.uint8)
-    binary_img = binary_img.astype('uint8')
-    binary_img = cv2.erode(binary_img, kernel, iterations=2)
-    return binary_img
 
 
 def get_mask_of_largest_connected_component(img_mask):
@@ -114,20 +75,44 @@ def get_mask_of_largest_connected_component(img_mask):
     return largest_mask
 
 
-def crop_img(img, img_mask):
-    farest_pixel = np.max(list(zip(*np.where(img_mask == 1))), axis=0)
-    nearest_pixel = np.min(list(zip(*np.where(img_mask == 1))), axis=0)
+def read_dicom_img(path):
+    dcm = pydicom.dcmread(path)
+    img = dcm.pixel_array
+    image = cv2.convertScaleAbs(img - np.min(img), alpha=(255.0 / min(np.max(img) - np.min(img), 10000)))
+
+    if dcm.PhotometricInterpretation == "MONOCHROME1":
+        image = np.invert(image)
+
+    image = image / 255.0
+
+    return image, image.shape
+
+
+def Image_procescing_mias(img_mias):
+    img_mias = skimage.exposure.equalize_adapthist(img_mias, clip_limit=0.03)
+    threshold = filters.threshold_isodata(img_mias)
+    bin_mias = (img_mias > threshold) * 1
+    kernel = np.ones((5, 5), np.uint8)
+    bin_mias = bin_mias.astype('uint8')
+    bin_mias = cv2.erode(bin_mias, kernel, iterations=-2)
+
+    img_mask_mias = get_mask_of_largest_connected_component(bin_mias)
+
+    farest_pixel = np.max(list(zip(*np.where(img_mask_mias == 1))), axis=0)
+    nearest_pixel = np.min(list(zip(*np.where(img_mask_mias == 1))), axis=0)
+
     if nearest_pixel[1] == 0:
-        return img[:farest_pixel[0], :farest_pixel[1]]
+        croped = img_mias[:farest_pixel[0], :farest_pixel[1]]
     else:
-        return img[nearest_pixel[0]:farest_pixel[0], nearest_pixel[1]:farest_pixel[1]]
+        croped = img_mias[nearest_pixel[0]:, nearest_pixel[1]:farest_pixel[1]]
+
+    return croped
 
 
 def main():
-    csv_file = 'C:\\Users\\Win10\\PycharmProjects\\QuantumResearch\\data\\csv\\INbreast.csv'
-    root_dir = 'C:\\Users\\Win10\\PycharmProjects\\QuantumResearch\\data\\dicom'
-    output_dir = 'C:\\Users\\Win10\\PycharmProjects\\QuantumResearch\\data\\png'
-
+    csv_file = 'C:\\Users\\Win10\\PycharmProjects\\QuantumResearch\\INbreast\\data\\csv\\INbreast.csv'
+    root_dir = 'C:\\Users\\Win10\\PycharmProjects\\QuantumResearch\\INbreast\\data\\dicom'
+    output_dir = 'C:\\Users\\Win10\\PycharmProjects\\QuantumResearch\\INbreast\\data\\png'
     size = 224
 
     preprocess_and_save_images(csv_file, root_dir, output_dir, size)
