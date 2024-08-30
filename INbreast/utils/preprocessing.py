51189bdd7cc482b2ba6ddb367b65bb62abbeba65
Yasmin Sobrinho <yasminrsobrinho@gmail.com>
2024-08-29 22:57:37 -0300

    Removing resize from preprocessing.py

--- INbreast/utils/preprocessing.py
+++ INbreast/utils/preprocessing.py
@@ -1,29 +1,25 @@
 import os
-import pydicom
 import cv2
+import pydicom
+import skimage
 import numpy as np
 import pandas as pd
 import scipy.ndimage
 from skimage import filters
-from sklearn.model_selection import train_test_split
-import skimage
 from skimage.color import rgb2gray
+from sklearn.model_selection import train_test_split
 
 
-def preprocess_and_save_images(csv_file, root_dir, output_dir, size=224, test_size=0.2):
-    print("Lendo o arquivo CSV...")
+def preprocess_and_save_images(csv_file, root_dir, output_dir, test_size=0.2):
     data = pd.read_csv(csv_file, sep=';')
     print(f"Total de entradas no CSV: {len(data)}")
 
     data = data[data['Bi-Rads'].notnull()]
-    print(f"Total de entradas após remover Bi-Rads nulos: {len(data)}")
 
     data['Label'] = data['Bi-Rads'].apply(lambda x: 0 if x in ['1', '2'] else 1)
-    print("Labels atribuídos com base no Bi-Rads.")
 
     data['File Name'] = data['File Name'].astype(str)
 
-    print("Dividindo os dados em conjuntos de treino e teste...")
     train_data, test_data = train_test_split(data, test_size=test_size, stratify=data['Label'], random_state=42)
     print(f"Total de imagens no treino: {len(train_data)}, no teste: {len(test_data)}")
 
@@ -51,23 +47,21 @@ def preprocess_and_save_images(csv_file, root_dir, output_dir, size=224, test_si
             print(f"Processando imagem DICOM: {dicom_file}")
             img, img_shape = read_dicom_img(dicom_path)
 
-            proc_img = Image_procescing_mias(img)
-
-            resized_img = cv2.resize(proc_img, (size, size))
+            proc_img = image_processing(img)
 
             label = labels_dict[file_name]
             print(f"Rótulo para {file_name}: {label}")
 
             label_dir = os.path.join(split_output_dir, str(label))
             output_path = os.path.join(label_dir, f"{file_name}.png")
-            cv2.imwrite(output_path, resized_img * 255)  # Multiplicando por 255 para garantir que a imagem esteja em escala de 0-255
+            cv2.imwrite(output_path, proc_img * 255)  # Multiplicando por 255 para garantir que a imagem esteja em escala de 0-255
             print(f"Imagem salva em: {output_path}")
 
     process_and_save(train_data, 'train', train_labels)
     process_and_save(test_data, 'test', test_labels)
 
 
-def get_mask_of_largest_connected_component(img_mask):
+def get_mask(img_mask):
     mask, num_labels = scipy.ndimage.label(img_mask)
     mask_pixels_dict = {i: np.sum(mask == i) for i in range(1, num_labels + 1)}
     largest_mask_index = max(mask_pixels_dict, key=mask_pixels_dict.get)
@@ -88,7 +82,7 @@ def read_dicom_img(path):
     return image, image.shape
 
 
-def Image_procescing_mias(img_mias):
+def image_processing(img_mias):
     img_mias = skimage.exposure.equalize_adapthist(img_mias, clip_limit=0.03)
     threshold = filters.threshold_isodata(img_mias)
     bin_mias = (img_mias > threshold) * 1
@@ -96,7 +90,7 @@ def Image_procescing_mias(img_mias):
     bin_mias = bin_mias.astype('uint8')
     bin_mias = cv2.erode(bin_mias, kernel, iterations=-2)
 
-    img_mask_mias = get_mask_of_largest_connected_component(bin_mias)
+    img_mask_mias = get_mask(bin_mias)
 
     farest_pixel = np.max(list(zip(*np.where(img_mask_mias == 1))), axis=0)
     nearest_pixel = np.min(list(zip(*np.where(img_mask_mias == 1))), axis=0)
@@ -113,9 +107,8 @@ def main():
     csv_file = 'C:\\Users\\Win10\\PycharmProjects\\QuantumResearch\\INbreast\\data\\csv\\INbreast.csv'
     root_dir = 'C:\\Users\\Win10\\PycharmProjects\\QuantumResearch\\INbreast\\data\\dicom'
     output_dir = 'C:\\Users\\Win10\\PycharmProjects\\QuantumResearch\\INbreast\\data\\png'
-    size = 224
 
-    preprocess_and_save_images(csv_file, root_dir, output_dir, size)
+    preprocess_and_save_images(csv_file, root_dir, output_dir)
 
 
 if __name__ == "__main__":
